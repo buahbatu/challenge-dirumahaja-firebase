ea55fb25158117e1ec2f82a2416f1cd358fbff2b
Archie <archie.isdiningrat@gmail.com>
2020-04-06 00:06:03 +0700

    update assets

--- pages/index.js
+++ pages/index.js
@@ -9,14 +9,8 @@ const Home = () => (
 
     <main>
       <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
+        #DirumahAja
       </h1>
-
-      <p className="description">
-        Get started by editing <code>pages/index.js</code>
-      </p>
-
-      <div className="grid">
         <a href="https://nextjs.org/docs" className="card">
           <h3>Documentation &rarr;</h3>
           <p>Find in-depth information about Next.js features and API.</p>
